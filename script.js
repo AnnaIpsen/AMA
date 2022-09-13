0913404ee6454935028b787150fff13ba0fb6c14
Artur Pilarek <artur.pilarek@gmail.com>
2022-09-13 12:58:16 +0200

    conflict resolved

--- script.js
+++
@@ -1,9 +0,0 @@
-const messages = ["O", "M", "G"]
-messages.forEach(message => console.log(message))
-
-console.log("Hello world!")
-console.log("Hello world!")
-console.log("Hello world!")
-console.log("Hello world!")
-
-console.log("date")
