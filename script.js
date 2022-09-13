7b9c7bea9f40f8c7f8beee9c1fe2c0f694c4ae1b
Anna-1902 <annaipsen19@gmail.com>
2022-09-13 12:49:05 +0200

    whatever

--- script.js
+++ script.js
@@ -6,8 +6,4 @@ console.log("Hello world!")
 console.log("Hello world!")
 console.log("Hello world!")
 
-<<<<<<< HEAD
 console.log("date")
-=======
-console.log('Anna speaking')
->>>>>>> b8a71f9fad584e1156c48d3105f53b54f029b6f4
